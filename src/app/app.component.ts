9c19190b2b4db7516279457bceb9a48be167264d
BStorm-Phil <philippe.haerens@bstorm.be>
2025-02-25 09:31:56 +0100

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  standalone: false,
+  styleUrl: './app.component.scss'
+})
+export class AppComponent {
+  title = 'TF_202502_BootcampNET__Angular';
+}
